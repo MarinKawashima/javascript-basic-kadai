b54e7bbc97a4da1ee3999d7962ea781eda54608d
MarinKawashima <149048639+MarinKawashima@users.noreply.github.com>
2023-11-13 11:59:45 +0900

    kadai_005をアップロード(JavaScript)

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 体重
+let bodyWeight= 68;
+
+// 身長
+let height= 1.7;
+
+// 計算式
+const BodyMassIndex= bodyWeight / (height*height);
+
+// コンソールへの出力
+console.log(bodyWeight);
+console.log(height);
+console.log(BodyMassIndex);
